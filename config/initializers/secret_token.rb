8287a7a04b86a4526801c041fcbdf3101460f6d5
dh <sojournrr@yahoo.com>
2013-03-10 20:33:07 -0400

    newer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cocform::Application.config.secret_token = '3b21d4712123c5ff1fe5d26030010b9c7538200cca6848750c33c93dd7534d2fc8766304c9dde8d3e4092386d89ed89730f5879b16dfa43288bfd132e1166144'
