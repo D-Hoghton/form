83579430976caaaec0221a6b81f6184dc127f11a
dh <sojournrr@yahoo.com>
2013-03-11 21:07:39 -0400

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Cocform::Application.routes.draw do
-  get "static_pages/home"
+                             # get "static_pages/home"
 
-  get "static_pages/contact"
+                             #        get "static_pages/contact"
+   root to: 'static_pages#home'
+
+   match '/contact', to: 'static_pages#contact'
+
 end
 
   # The priority is based upon order of creation:
